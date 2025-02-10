7b1550521c96fce56513f8e29517c4598f612deb
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-02-10 21:08:14 +0300

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/C - Sura loves coding 254240.py
@@ -0,0 +1,20 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+n = int(input())
+s = input().strip()
+word = []
+
+for i in range(n-1, -1, -1):
+  letter = s[i]
+
+  length = len(word)
+
+  if length % 2 == 0:
+    pos = length // 2
+  
+  else:
+    pos = length // 2
+
+  word.insert(pos, letter)
+original = ''.join(word)
+print(original)
