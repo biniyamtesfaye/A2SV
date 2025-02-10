6c9d3b87f1899a69103204bd1a4c70e201dc9866
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-02-10 20:44:48 +0300

    Created 'B - The special paintbrush' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/B - The special paintbrush 254199.py
@@ -0,0 +1,19 @@
+# Problem: B - The special paintbrush - https://codeforces.com/gym/586622/problem/B
+
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  s = input()
+
+  firstBlack = -1
+  lastBlack = -1
+
+  for i in range(n):
+    if s[i] == 'B':
+      if firstBlack == -1:
+        firstBlack = i
+      lastBlack = i
+
+  minLength = lastBlack -firstBlack + 1
+  print(minLength)
