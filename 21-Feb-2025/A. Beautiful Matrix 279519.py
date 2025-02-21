63794341242211fc7990d2680eb06ae04f40093a
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-02-21 21:48:46 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279519.py
@@ -0,0 +1,15 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = []
+for _ in range(5):
+    row = list(map(int, input().split(" ")))
+    matrix.append(row)
+        
+for r in range(5):
+    for c in range(5):
+        if matrix[r][c] == 1:
+            one_r = r
+            one_c = c
+
+distance = abs(one_c - 2) + abs(one_r - 2)
+print(distance)
