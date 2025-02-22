f984ecf91fc2e98708da6e911793670b40cf66b5
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-02-22 22:57:39 +0300

    Created 'Maximum Sum of an Hourglass - LeetCode' by A2SV Hub

---
+++ 22-Feb-2025/Maximum Sum of an Hourglass - LeetCode 280548.py
@@ -0,0 +1,29 @@
+# Problem: Maximum Sum of an Hourglass - LeetCode - https://leetcode.com/problems/maximum-sum-of-an-hourglass/description/
+
+class Solution(object):
+    def isBound(self, r, c, rows, cols):
+        return 0 <= r < rows and 0 <= c < cols
+    def isHourglass(self, r, c, rows, cols):
+        return 1 <= r < rows -1 and 1 <= c < cols - 1
+    
+    def maxSum(self, grid):
+        directions = {
+            (-1, -1), (-1,0), (-1, 1),(1,-1),(1,0),(1,1),(0,0)
+            }
+        rows, cols = len(grid), len(grid[0])
+        max_sum = float('-inf')
+        for r in range(rows):
+            for c in range(cols):
+                if self.isHourglass(r, c,rows,cols):
+                    hourglass = [
+                        grid[r + dx][c + dy]
+                        for dx, dy in directions
+                        if self.isBound(r + dx, c + dy, rows, cols)
+                    ]
+                    hourglass_sum = sum(hourglass)
+
+                    max_sum = max(max_sum, hourglass_sum)
+
+        return max_sum
+
+        
