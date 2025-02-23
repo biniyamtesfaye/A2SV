1b792e99d6b2e04d2c13ba8f70bd0d9829a4f311
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-02-23 23:56:31 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 23-Feb-2025/Chat Order 281356.py
@@ -0,0 +1,14 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+friends = [input().strip() for _ in range(n)]
+
+chatted = set()
+result = []
+
+for person in reversed(friends):
+    if person not in chatted:
+        result.append(person)
+        chatted.add(person)
+
+print("\n".join(result))
