efc11c9390bf2385c513694744688acae9744cab
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-02-24 21:40:54 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 24-Feb-2025/Fox And Snake 281918.py
@@ -0,0 +1,11 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = map(int, input().split())  
+
+for row in range(1, n + 1):  
+    if row % 2 == 1:  
+        print("#" * m)  
+    elif (row // 2) % 2 == 1:  
+        print("." * (m - 1) + "#")  
+    else:  
+        print("#" + "." * (m - 1))  
