7404387204be476b24cf93cb2806cfeb11bec4f7
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-03-11 22:08:26 +0300

    Created 'String Compression' by A2SV Hub

---
+++ 11-Mar-2025/String Compression 303215.py
@@ -0,0 +1,25 @@
+# Problem: String Compression - https://leetcode.com/problems/string-compression/
+
+class Solution:
+   def compress(self, chars):
+        write = 0  
+        read = 0  
+
+        while read < len(chars):
+            char = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == char:
+                count += 1
+                read += 1
+            
+            chars[write] = char
+            write += 1
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+
+        return write  
+    
