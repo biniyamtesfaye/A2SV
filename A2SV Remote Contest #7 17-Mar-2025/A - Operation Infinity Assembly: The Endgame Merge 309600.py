9997a7346ba77f09247319e9d09b117bf96faede
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-03-17 21:21:16 +0300

    Created 'A - Operation Infinity Assembly: The Endgame Merge' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/A - Operation Infinity Assembly: The Endgame Merge 309600.py
@@ -0,0 +1,25 @@
+# Problem: A - Operation Infinity Assembly: The Endgame Merge - https://codeforces.com/gym/596004/problem/A
+
+t = int(input())
+
+for _ in range(t):
+  n, m, k = map(int, input().split())
+  a = sorted(input().strip())
+  b = sorted(input().strip())
+
+  i, j = 0, 0
+  count_a, count_b = 0, 0
+  c = []
+
+  while i < n and j < m:
+    if (count_a < k and (count_b == k or a[i] < b[j])):
+      c.append(a[i])
+      i += 1
+      count_a += 1
+      count_b = 0
+    else:
+      c.append(b[j])
+      j += 1
+      count_b += 1
+      count_a = 0
+  print("".join(c))
