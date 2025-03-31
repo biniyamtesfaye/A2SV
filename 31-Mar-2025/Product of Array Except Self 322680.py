164c7c7dd721ff602d6fb87c1ea5598e83a5c89d
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-03-31 19:30:49 +0300

    Created 'Product of Array Except Self' by A2SV Hub

---
+++ 31-Mar-2025/Product of Array Except Self 322680.py
@@ -0,0 +1,23 @@
+# Problem: Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
+
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [1] * n
+        
+        # Compute prefix products
+        prefix_product = 1
+        for i in range(n):
+            result[i] = prefix_product
+            prefix_product *= nums[i]
+        
+        # Compute suffix products and update result
+        #Code by Binyam
+        suffix_product = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= suffix_product
+            suffix_product *= nums[i]
+        
+        return result
