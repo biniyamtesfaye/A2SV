b48828d0c17b691d30838a71d11e6048c7f348f0
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-04-02 21:03:54 +0300

    Created 'Missing Number' by A2SV Hub

---
+++ 02-Apr-2025/Missing Number 324219.py
@@ -0,0 +1,12 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        v = [-1] * (n + 1)
+        for num in nums:
+            v[num] = num
+        for i in range(len(v)):
+            if v[i] == -1:
+                return i
+        return 0
