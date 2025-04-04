09d6b010d0ecaf89c5ee71537ba9afe674875f89
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-04-04 22:46:27 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 04-Apr-2025/Kefa and Company 326447.py
@@ -0,0 +1,22 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+n, d = map(int, input().split())
+friends = []
+for _ in range(n):
+    mi, si = map(int, input().split())
+    friends.append((mi, si))
+friends.sort()
+
+max_total = 0
+current_sum = 0
+left = 0
+
+for right in range(n):
+    current_sum += friends[right][1]
+    while friends[right][0] - friends[left][0] >= d:
+        current_sum -= friends[left][1]
+        left += 1
+    if current_sum > max_total:
+        max_total = current_sum
+
+print(max_total)
