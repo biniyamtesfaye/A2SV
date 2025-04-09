8b8d7a4e7b869d761c277de69022c2c60b80a1c0
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-04-09 21:54:58 +0300

    Created 'Count Vowel Substrings of a String' by A2SV Hub

---
+++ 09-Apr-2025/Count Vowel Substrings of a String 330146.py
@@ -0,0 +1,11 @@
+# Problem: Count Vowel Substrings of a String - https://leetcode.com/problems/count-vowel-substrings-of-a-string/description/
+
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        v={'a','e','i','o','u'}
+        c=0
+        for i in range(len(word)-4):
+            for j in range(i+5,len(word)+1):
+                if set(word[i:j])==v:
+                    c+=1
+        return c
