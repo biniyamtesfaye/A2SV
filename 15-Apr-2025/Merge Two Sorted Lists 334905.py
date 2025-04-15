1a6f303518d9873ae3c730b7f18ca5e252a72bd0
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-04-16 00:00:58 +0300

    Created 'Merge Two Sorted Lists' by A2SV Hub

---
+++ 15-Apr-2025/Merge Two Sorted Lists 334905.py
@@ -0,0 +1,16 @@
+# Problem: Merge Two Sorted Lists - https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:   
+        dummy = temp = ListNode(0)
+        while l1 != None and l2 != None: #1
+
+            if l1.val < l2.val: #2
+                temp.next = l1 #3
+                l1 = l1.next #4
+            else: 
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        temp.next = l1 or l2  #5
+        return dummy.next #6
