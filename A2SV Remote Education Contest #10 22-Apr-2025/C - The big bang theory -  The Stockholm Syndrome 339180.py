9b04aa631120eb12d1f1ec43328880a521c82bd8
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-04-22 23:21:00 +0300

    Created 'C - The big bang theory -  The Stockholm Syndrome' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 22-Apr-2025/C - The big bang theory -  The Stockholm Syndrome 339180.py
@@ -0,0 +1,10 @@
+# Problem: C - The big bang theory -  The Stockholm Syndrome - https://codeforces.com/gym/604857/problem/C
+
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+mex = 1
+for x in a:
+    if x >= mex:
+        mex += 1
+print(mex)
