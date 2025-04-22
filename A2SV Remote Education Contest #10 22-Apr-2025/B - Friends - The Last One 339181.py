64e32f29aea372f53ce3a9b5efb737b9e1999eaa
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-04-22 23:21:39 +0300

    Created 'B - Friends - The Last One' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 22-Apr-2025/B - Friends - The Last One 339181.py
@@ -0,0 +1,15 @@
+# Problem: B - Friends - The Last One - https://codeforces.com/gym/604857/problem/B
+
+t = int(input())
+res = []
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    s = sum(a)
+    mx = max(a)
+    mn = min(a)
+    if s + mx - mn + n <= m:
+        res.append("YES")
+    else:
+        res.append("NO")
+print("\n".join(res))
