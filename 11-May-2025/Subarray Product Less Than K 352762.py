cd8121d001eabc0bc0a9d7aa15bd30dc28437778
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-05-11 21:23:51 +0300

    Created 'Subarray Product Less Than K' by A2SV Hub

---
+++ 11-May-2025/Subarray Product Less Than K 352762.py
@@ -0,0 +1,19 @@
+# Problem: Subarray Product Less Than K - https://leetcode.com/problems/subarray-product-less-than-k/
+
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0  
+
+        product = 1
+        left = 0
+        count = 0
+
+        for right in range(len(nums)):
+            product *= nums[right]
+            while product >= k and left <= right:
+                product //= nums[left]
+                left += 1
+            count += right - left + 1
+
+        return count
