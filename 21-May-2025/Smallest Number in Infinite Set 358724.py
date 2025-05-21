66dae7ad27e86a0d974d5fa0d8a36d7ffc64cb56
biniyamtesfaye <binyamtesfaye931@gmail.com>
2025-05-21 11:50:00 -0700

    Created 'Smallest Number in Infinite Set' by A2SV Hub

---
+++ 21-May-2025/Smallest Number in Infinite Set 358724.py
@@ -0,0 +1,23 @@
+# Problem: Smallest Number in Infinite Set - https://leetcode.com/problems/smallest-number-in-infinite-set/description/
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.nums = list(range(1,1001))
+        
+
+    def popSmallest(self) -> int:
+        self.nums.sort()
+        n = self.nums[0]
+        self.nums[:] = self.nums[1:]
+        return n
+
+    def addBack(self, num: int) -> None:
+        if num not in self.nums:
+            self.nums.append(num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
